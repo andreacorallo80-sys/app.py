b15dc73ae786cd79a4ac14cd45976fb5e74b4de1
andreacorallo80-sys <andrea.corallo.80@gmail.com>
2026-02-08 17:01:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
 
-col1, col2, col3 = st.columns([1,1,1])
-with col2: # Lo mettiamo nella colonna centrale per centrarlo
-    st.image("https://www.abbonamentiauto.it/wp-content/uploads/2023/04/Logo-Abbonamenti-Auto-2.png", width=250)
-
-st.title("🛡️ Smart Cost Analyzer")
-
-# Nuovo Titolo della WebApp
-st.title("🛡️ Smart Cost Analyzer")
+st.title("🛡️ Smart Cost Analyzer") Smart Cost Analyzer")
 st.markdown("##### Powered by **abbonamentiauto.it**")
 st.markdown("---")
 # Configurazione Pagina
